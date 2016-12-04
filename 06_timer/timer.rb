7b25931b41b53f08f40e7d858bb7756eb8d04e9a
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-04 02:12:59 -0500

    completes 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    delimiter = ":"
+    hours = @seconds / 3600
+    minutes = (@seconds % 3600) / 60
+    seconds = (@seconds % 3600) % 60
+
+    return (padded(hours) + delimiter + padded(minutes) \
+            + delimiter + padded(seconds))
+  end
+
+  def padded(digits)
+    if digits >= 10
+      return digits.to_s
+    else
+      return '0' + digits.to_s
+    end
+  end
+
 end
